b97ac517704ff2cab1dc4a92e31a1060007a100f
Tim Uy <tim@loqu8.com>
2015-12-03 01:57:10 -0800

    setup Nancy project with error

---
+++ nsWeb/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace nsWeb.Models
+{
+    public class Person
+    {
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        public Wallet Wallet { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ nsWeb/Nancy/PersonModule.cs
@@ -0,0 +1,21 @@
+﻿using Nancy;
+using nsWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace nsWeb.Nancy
+{
+    public class PersonModule : NancyModule
+    {
+        public PersonModule()
+        {
+            Get["/"] = _ =>
+            {
+                var person = new Person("Andreas");  // the wallet is null
+                return View["person", person];
+            };
+        }
+    }
+}
